a407b900739572bde9cf679da7334229e36b59e1
duncan60 <tmduncan60@gmail.com>
2016-02-25 17:36:50 +0800

    update practice3

---
+++ test/practice3.test.js
@@ -0,0 +1,47 @@
+import { expect } from 'chai';
+import { updateDate } from '../src/core.js';
+
+describe('practice 2', () => {
+    before(() => {
+        console.log('[ test suite start ]');
+    });
+
+    after(() => {
+        console.log('[ test suite end ]');
+    });
+
+    beforeEach(() => {
+        console.log('  @test case start');
+    });
+
+    afterEach(() => {
+        console.log('  @test case end');
+    });
+    let book = {
+        name : 'unit test',
+        price: 100
+    };
+    it('is object', () => {
+        expect(book).to.be.an('object');
+    });
+    it('object update', () => {
+        let update = {
+            name: 'redux unit test',
+            author: {
+                firstName: 'Albert',
+                lastName : 'Einstein'
+            }
+        },
+        newBooks = updateDate(book, update);
+        expect(newBooks).to.deep.equal({
+            name  : 'redux unit test',
+            price : 100,
+            author: {
+                firstName: 'Albert',
+                lastName : 'Einstein'
+            }
+        });
+        expect(newBooks).to.have.deep.property('author.lastName', 'Einstein');
+
+    });
+});
