bf1b87e9dadbd65658b801b30089182869d0939b
duncan60 <tmduncan60@gmail.com>
2016-02-25 17:47:59 +0800

    pass testing

--- test/todo-practice.test.js
+++ test/todo-practice.test.js
@@ -5,27 +5,27 @@ practice
     將 phone 初始值在 before 中處理
 */
 
-// import { expect } from 'chai';
-// import { updateDate } from '../src/core.js';
+import { expect } from 'chai';
+import { updateDate } from '../src/core.js';
 
-// describe('todo practice', () => {
-//     let phone;
-//     before(() => {
-//         phone = {
-//             type   : 'iPhone6s',
-//             display: '4-7',
-//             storage: '64GB'
-//         };
-//     });
-//     it('has all keys', () => {
-//         expect(phone).to.not.be.empty;
-//         expect(phone).to.have.all.keys('type', 'display', 'storage');
-//     });
-//     it('update phone', () => {
-//         let update = {
-//             type: 'iPhone6sPlus'
-//         },
-//         newPhone = updateDate(phone, update);
-//         expect(newPhone).to.have.deep.property('type', 'iPhone6sPlus');
-//     });
-// });
+describe('todo practice', () => {
+    let phone;
+    before(() => {
+        phone = {
+            type   : 'iPhone6s',
+            display: '4-7',
+            storage: '64GB'
+        };
+    });
+    it('has all keys', () => {
+        expect(phone).to.not.be.empty;
+        expect(phone).to.have.all.keys('type', 'display', 'storage');
+    });
+    it('update phone', () => {
+        let update = {
+            type: 'iPhone6sPlus'
+        },
+        newPhone = updateDate(phone, update);
+        expect(newPhone).to.have.deep.property('type', 'iPhone6sPlus');
+    });
+});
